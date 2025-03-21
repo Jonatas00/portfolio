cbeec848cf6bae41809399fe841c457fd90808a3
Jonatas <djonatas31@gmail.com>
2025-03-20 21:16:23 -0300

    🔧fix view on mobile

--- scripts/Scroll.js
+++ scripts/Scroll.js
@@ -14,7 +14,7 @@ document.addEventListener("scroll", () => {
   const containers = document.querySelectorAll(".container");
   containers.forEach((container) => {
     const rect = container.getBoundingClientRect();
-    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+    if (rect.bottom >= 0 && rect.top <= window.innerHeight) {
       container.classList.remove("hidden");
     } else {
       container.classList.add("hidden");
